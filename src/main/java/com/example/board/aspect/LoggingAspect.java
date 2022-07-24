53ff53d416f8f4759ad8f589341a73a515df5111
Amenable-C <duswo0624@naver.com>
2022-07-24 10:21:47 +0900

    Feat : Add AOP function by making LoggingAspect.java

---
+++ src/main/java/com/example/board/aspect/LoggingAspect.java
@@ -0,0 +1,39 @@
+package com.example.board.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* com.example.board.service.*.get*(..))")
+    public void loggerBefore(){
+        System.out.println("get 으로 시작하는 메서드 시작");
+    }
+
+    @After("execution(* com.example.board.service.*.get*(..))")
+    public void loggerAfter(){
+        System.out.println("get 으로 시작하는 메서드 끝");
+    }
+
+    @Around("execution(* com.example.board.controller.UserController.*(..))")
+    public Object loggerAround(ProceedingJoinPoint pjp) throws Throwable{
+        long beforeTimeMillis = System.currentTimeMillis();
+        System.out.println("[UserController] 실행시작 : "
+                +pjp.getSignature().getDeclaringTypeName() + "."
+                +pjp.getSignature().getName());
+        Object result = pjp.proceed();
+
+        long afterTimeMillis = System.currentTimeMillis() - beforeTimeMillis;
+        System.out.println("[UserController] 실행 완료 : " + afterTimeMillis + "밀리초 소요"
+                +pjp.getSignature().getDeclaringTypeName() + "."
+                +pjp.getSignature().getName());
+
+        return result;
+    }
+}
